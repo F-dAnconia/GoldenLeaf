ffee34da7a7af1c006e02e4497099bfa194bf41b
F-dAnconia <Lenny3@yandex.ru>
2017-02-18 21:17:55 +0200

    frontend basic part2

---
+++ views/site/signup.php
@@ -0,0 +1,53 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+use dosamigos\datepicker\DatePicker;
+
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-signup">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to signup:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'surname') ?>
+
+                <?= $form->field($model, 'email') ?>
+
+                <?= $form->field($model, 'birth_date')->widget(
+                    DatePicker::className(), [
+                    // inline too, not bad
+                    'inline' => false,
+                    // modify template for custom rendering
+//                    'template' => '<div class="well well-sm" style="background-color: #fff; width:250px">{input}</div>',
+                    'clientOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-M-d'
+                    ]
+                ]);?>
+
+                <?= $form->field($model, 'phone') ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
